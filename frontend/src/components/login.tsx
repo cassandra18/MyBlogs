79b6bc1dfcb8bef2874d2029cbea6529bdbffbdc
cassandra18 <cassandralelei013@gmail.com>
2024-03-28 16:16:38 +0300

    updated the email id in the login component

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -52,7 +52,7 @@ const LoginForm: React.FC = () => {
           </label>
           <input
             className="shadow appearance-none rounded w-full py-2 px-3 text-gray leading-tight focus:outline-none focus:shadow-outline border"
-            id="email"
+            id="email-input"
             type="email"
             placeholder="abc@email.com"
             value={email}
